0017480ef6f529bfca149918b490ec8c3dcf3384
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-16 21:10:45 +0300

    feat: adds Admin model and password_hash

--- server/models.py
+++ server/models.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from flask_bcrypt import Bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
+
 
 db=SQLAlchemy()
+bcrypt = Bcrypt()
+
 
 class Patient(db.Model,SerializerMixin):
     __tablename__='patients'
@@ -19,6 +24,24 @@ class Patient(db.Model,SerializerMixin):
 
     serialize_rules=('-appointments.patient',)
 
+class Admin(db.Models,SerializerMixin):
+    __tablename__ = "admins"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    @hybrid_property
+    def password_hash(self):
+        raise ValueError("password hash cannot be viewed")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
 
 class Appointment(db.Model,SerializerMixin):
     __tablename__='appointments'
