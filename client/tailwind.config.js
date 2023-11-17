e2ac3b9f3a5517ba81d7e5d8d96b9cc88f2acab6
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-17 15:38:32 +0300

    installations

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'primary': ['Poppins'],
+        'secondary': ['Nunito'],
+        'tertiary':['Montserrat']
+      }
+    },
   },
   plugins: [],
 }
