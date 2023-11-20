7d86dc24ec5374189ee02a2f3e66ff217ddeb83a
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-20 11:22:23 +0300

    ammends footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,12 +5,12 @@ import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
 const Footer = () => {
 
     return ( <>
-<div className='bg-color-tertiary'>
-<div className='footer flex mx-60 p-10'>
-    <div className='address w-1/4'>
+
+<div className='footer flex flex-wrap flex-row justify-evenly p-10'>
+    <div className='address mt-3'>
         <div className='flex'>
             <HealthAndSafetyIcon className='text-blue-500 my-1'/>
-            <h1 className='text-2xl  text-indigo-800'>ABCLINIC</h1>
+            <h1 className='text-2xl  text-indigo-800 mb-1'>ABCLINIC</h1>
         </div>
         <ul className='flex flex-col mr-4'>
             <li>1234 Nairobi</li>
@@ -19,8 +19,8 @@ const Footer = () => {
             <li>Contact: info@abc.com</li>
         </ul>
     </div>  
-    <div className='about w-1/4'>
-         <h1 className='text-2xl text-indigo-800'>About Us</h1>
+    <div className='about mt-3'>
+         <h1 className='text-2xl text-indigo-800 mb-1'>About Us</h1>
         <ul className='flex flex-col mr-4'>
             <li>Our Mission & Values</li>
             <li>Transformation</li>
@@ -28,8 +28,8 @@ const Footer = () => {
             <li>Publicationc & Reports</li>
         </ul>
     </div>
-    <div className='about w-1/4'>
-            <h1 className='text-2xl text-indigo-800'>Our Services</h1>
+    <div className='about mt-3'>
+            <h1 className='text-2xl text-indigo-800 mb-1'>Our Services</h1>
         <ul className='flex flex-col mr-4'>
             <li>Lung Diseases</li>
             <li>Orthopaedic</li>
@@ -37,16 +37,16 @@ const Footer = () => {
             <li>Dental Services</li>
         </ul>
     </div>
-    <div className='time w-1/4'>
-        <h1 className='text-2xl text-indigo-800'>Hospital Time    </h1>
+    <div className='time mt-3'>
+        <h1 className='text-2xl text-indigo-800 mb-1'>Hospital Time    </h1>
         <div className='flex'>
-            <ul className='flex flex-col mr-4 text-purple-800' >
-                <li>Monday-Friday</li>
+            <ul className='flex flex-col mr-2 text-purple-800 ' >
+                <li>Mon-Fri</li>
                 <li>Saturday</li>
                 <li>Sunday</li>
                 <li>Holidays</li>
             </ul>
-            <ul className='flex flex-col mr-4 text-gray-800'>
+            <ul className='flex text-gray-800 flex-col'>
                 <li>08:00-20:00</li>
                 <li>09:00-18:00</li>
                 <li>09:00-18:00</li>
@@ -55,15 +55,14 @@ const Footer = () => {
         </div>
     </div>
 </div>
-</div>
-    <div className="socials flex justify-around bg-color-secondary px-10 py-2" >
-         <div className="mt-4 text-base text-white font-bold">&copy; ABClinic 2023. All rights reserved.</div>
-         <div className='flex items-center justify-around w-1/4'>
-            <FaYoutube size={32} color='#FF0000' />
-            <FaFacebook size={32} color='#3b5998'/>
-            <FaTwitter size={32} color='##1D9BF0'/>
-            <FaLinkedin size={32} color='#0077b5'/>
-            <FaInstagram size={32} color='#e4405f'/>
+    <div className="socials flex flex-row justify-around bg-color-secondary px-10 py-4" >
+         <div className=" text-base text-white font-bold w-1/2 mx-60">&copy; ABClinic 2023. All rights reserved.</div>
+         <div className='flex items-center justify-evenly w-1/2 text-color-white text-3xl mr-60'>
+            <FaYoutube />
+            <FaFacebook />
+            <FaTwitter />
+            <FaLinkedin />
+            <FaInstagram />
          </div>
     </div>
     </> );
