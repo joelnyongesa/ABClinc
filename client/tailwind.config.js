8605af2cbf3dc01c07c5d58ff24a77763ea60e6c
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-20 09:59:30 +0300

    changes colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,6 +10,13 @@ export default {
         'primary': ['Poppins'],
         'secondary': ['Nunito'],
         'tertiary':['Montserrat']
+      },
+      colors:{ 
+        "color-primary": "#313234",
+        "color-secondary":"#5447E5",
+        "color-tertiary": "#F1F5FF",
+        "color-white": "#FCFDFF"
+        
       }
     },
   },
